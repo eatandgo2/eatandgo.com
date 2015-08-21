4acd71a91e1b6c082ddc4bec137d80a63c964f82
MyDuongThi <12520266@gm.uit.edu.vn>
2015-08-21 20:18:08 +0700

    Content_Vung.php

---
+++ content-vung.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatiple" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Cẩm nang du lịch Việt Nam</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/stylemenu.css">
+	<link rel="stylesheet" href="css/styledropdown.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="carousel.css" rel="stylesheet">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	 <script src="js/jquery-1.11.3.min.js"></script>
+	  <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+	 <style>
+	 	body{
+	 		background: url(image/bg_body.png);
+	 	}
+	 	.container .row .col-md-9{
+	 		
+	 		background-color: white !important;
+	 	}
+	 	.container{
+	 		margin-top: 10px;
+	 	}
+	 	.item-detail{
+	 		margin-top: 10px;
+	 	}
+
+	 </style>
+</head>
+<body>
+<!-- MENU -->
+<nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" 
+        	aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+          	</button>
+            <a class="navbar-brand" href="#">Du Lịch Việt Nam</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse navbar-menubuilder">
+          	<ul class="nav navbar-nav">
+            	<li><a href="#">Trang Chủ</a></li>
+            	<li class="dropdown">
+            		<a href="#" data-toggle="dropdown">Du Lịch Miền Bắc <span class="caret"></span></a>
+            		<ul class="dropdown-menu">
+							<li><a href="#">Du Lịch Hạ Long</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Sapa</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Hà Nội</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Đông Bắc</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Tây Bắc</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Ninh Bình</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Quảng Ninh</a></li>
+					</ul>
+            	</li>
+            </ul>
+            <ul class="nav navbar-nav">
+            	<li class="dropdown">
+            		<a href="#" data-toggle="dropdown">Du Lịch Miền Trung <span class="caret"></span></a>
+            		<ul class="dropdown-menu">
+							<li><a href="#">Du Lịc Huế</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Nha Trang</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Đà Lạt</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Hội An</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Đà Nẵng</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Phú Yên</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Tây Nguyên</a></li>
+					<!-- </ul class="dropdown-menu"> -->
+					</ul>
+            	</li>
+            </ul>
+            <ul class="nav navbar-nav">
+            	<li class="dropdown">
+            		<a href="#" data-toggle="dropdown">Du Lịch Miền Nam <span class="caret"></span></a>
+            		<ul class="dropdown-menu">
+							<li><a href="#">Du Lịch Côn Đảo</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Phú Quốc</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Bà Rịa - Vũng Tàu</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Bạc Liêu</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Tiền Giang</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Du Lịch Cần Thơ</a></li>
+					</ul>
+            	</li>
+            	<li><a href="#">Ẩm Thực Vùng Miền</a></li>
+            	<li><a href="#">Mẹo Hay</a></li>
+            	<li><a href="#">Bản Đồ Du Lịch</a></li>
+          	</ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+<!-- END MENU -->
+
+<!-- SLIDE  SHOW-->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+        <img src="image/baner1.jpg" alt="Chania" height="300px">
+        <div class="container">
+            <div class="carousel-caption">
+              <h1>Du Lịch Việt Nam - Nơi chia sẻ mọi thông tin du lịch hot nhất</h1>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+<!-- END SLIDE SHOW-->
+<div class="container">
+	<div class="row">
+				<!-- SIDEBAR -->
+	  	<div class="col-md-3 " class="list-vung">
+	  		<div class="list-group">
+			  <a href="#" class="list-group-item active">DU LỊCH MIỀN BẮC</a>
+			  <a href="#" class="list-group-item">Du lịch Hạ Long</a>
+			  <a href="#" class="list-group-item">Du lịch Sapa</a>
+			  <a href="#" class="list-group-item">Du lịch Hà Nội</a>
+			  <a href="#" class="list-group-item">Du lịch Đông Bắc</a>
+			  <a href="#" class="list-group-item">Du lịch Tây Bắc</a>
+			  <a href="#" class="list-group-item">Du lịch Ninh Bình</a>
+			  <a href="#" class="list-group-item">Du lịch Quảng Ninh</a>
+
+			</div>
+	  	</div>
+	  	<div class="col-md-9">
+		  	<div class="item-detail">
+			  	<div class="row">
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+	      		</div><!-- /.row -->
+	      	</div>
+	      	<hr/>
+      		<div>
+			  	<div class="row">
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+	      		</div><!-- /.row -->
+	      	</div>
+	      	<hr/>
+      		<div >
+			  	<div class="row">
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+	      		</div><!-- /.row -->
+	      	</div>
+	      	<hr/>
+      		<div >
+			  	<div class="row">
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+			        <div class="col-lg-4">
+			          	<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="100%" height="140">
+			          	<h2><a href="#">Heading</a></h2>
+			          	<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			          	
+			        </div><!-- /.col-lg-4 -->
+	      		</div><!-- /.row -->
+	      	</div>
+
+      		<div class="page" style="background-color: #cecece; text-align: center; margin-bottom: 10px;" >
+					<nav>
+					  	<ul class="pagination">
+						    <li>
+						      <a href="#" aria-label="Previous">
+						        <span aria-hidden="true">&laquo;</span>
+						      </a>
+						    </li>
+						    <li><a href="#">1</a></li>
+						    <li><a href="#">2</a></li>
+						    <li><a href="#">3</a></li>
+						    <li><a href="#">4</a></li>
+						    <li><a href="#">5</a></li>
+						    <li><a href="#">6</a></li>
+						    <li><a href="#">7</a></li>
+
+						    <li>
+							      <a href="#" aria-label="Next">
+							        <span aria-hidden="true">&raquo;</span>
+							      </a>
+					    	</li>
+					  	</ul>
+					</nav>
+				</div>
+
+
+
+		</div><!-- End Main-Content -->
+	</div>
+</div>
+
+<!-- END CONTENT-->
+
+<!-- END CONTENT-WRAP-->
+
+<!-- FOOTER -->
+<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="line-col"></div>
+				<ul>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<div class="line-col"></div>
+				<ul>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<div class="line-col"></div>
+				<ul>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<div class="line-col"></div>
+				<ul>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+					<li>
+						<img src="#" alt="Đây là cái ảnh">
+						<a href="#" title="">Link tới địa điểm trên ảnh</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="line"></div>
+		<!-- <div class="line-col"></div> -->
+	</div>
+</footer>
+<!-- END FOOTER -->
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="jquery-1.11.3.min.js"></script>
+<script src="../../assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
